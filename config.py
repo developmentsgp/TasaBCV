c21afe318b89c52d4df4858f25985c5d0d3da9f4
Desarrollos GP <developmentsgp@gmail.com>
2025-06-09 19:25:40 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TELEGRAM_TOKEN = "6313252063:AAHJblx8ncxWqJKoQmaTzPRcqIvaYy6ph3U"
+urlBCV = 'https://www.bcv.org.ve/'
+idDolar = "dolar"
+idEuro = "euro"
+
+urlApiSAP = "https://enlaces.fortiddns.com:44301/sap/bc/zapi_regrate?sap-client=400"
+SAPUser = "WSAPIREST"
+SAPPassword = "APIR3st@123"
+
